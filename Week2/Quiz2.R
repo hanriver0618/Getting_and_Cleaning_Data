8b2ffb5a4af17e9d25708ceac58abe748f1d3dbc
hanriver0618 <hanriver0618@gmail.com>
2017-02-08 19:26:25 -0500

    Update Quiz2.R

--- Week2/Quiz2.R
+++ Week2/Quiz2.R
@@ -37,6 +37,7 @@ for (i in 1:length(result)){
 ## Which of the following commands will select only the data for the probability 
 ## weights pwgtp1 with ages less than 50?
 
+library(sqldf)
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
 download.file(fileUrl,destfile="data.csv",method="curl")
 acs<-read.csv("data.csv")
